09a897e3cac2b7655243e5729e25060e98207faa
dvdmarveira <ddmdoliveira@gmail.com>
2025-06-21 17:12:09 -0300

    feat: standardizes data input and output in the request

--- src/routes/auth_routes.py
+++ src/routes/auth_routes.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from src.models.models import User
 from src.services.session_dependencies_service import get_session
 from main import bcrypt_context
+from src.schemas.user_schema import UserSchema
+from sqlalchemy.orm import Session
 
 auth_router = APIRouter(prefix="/api/auth", tags=["auth"])
 
@@ -13,15 +15,15 @@ async def authenticate():
   return {"oi"}
 
 @auth_router.post("/sign_up")
-async def sign_up(name: str, email: str, password: str, gender: str, session = Depends(get_session)):
+async def sign_up(user_schema: UserSchema, session: Session = Depends(get_session)):
 
-  user = session.query(User).filter(User.email==email).first()
+  user = session.query(User).filter(User.email==user_schema.email).first()
   if user:
     # Já existe usuário com esse e-mail
-    return {"message": "email already exists"}
+    raise HTTPException(status_code=400, detail="email already exists")
   else:
-    cryptography_password = bcrypt_context.hash(password)
-    new_user = User(name, email, cryptography_password, gender)
+    cryptography_password = bcrypt_context.hash(user_schema.password)
+    new_user = User(user_schema.name, user_schema.email, cryptography_password, user_schema.gender, user_schema.admin, user_schema.active)
     session.add(new_user)
     session.commit()
-    return {"message": "user registered successfully"}
+    return {"message": f"user registered successfully {user_schema.email}"}
