71ef41a65914a436dc97e9d5da93c3e88ce49dea
dvdmarveira <ddmdoliveira@gmail.com>
2025-06-22 01:03:59 -0300

    refactor: remove environment variables

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
 from fastapi import FastAPI
-from passlib.context import CryptContext
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-SECRET_KEY = os.getenv("SECRET_KEY")
 
 app = FastAPI()
-
-bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 from src.routes.auth_routes import auth_router
 from src.routes.order_routes import order_router
